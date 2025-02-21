eb9fce21727ffda29272aecbb47f634d218ad734
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2025-02-21 15:00:19 -0600

    Reinicio del proyecto en version2

--- login/cajero/proteccion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-error_reporting(0);
-$varsesion = $_SESSION['nombre'];
-if($varsesion == null || $varsesion=''){
-    header("location: ../loginusuario.php");
-    die();
-}
-?>
--- login/finpagadmin.php
+++
@@ -1,5 +0,0 @@
-<div class="lineanegra2"></div>
-<div class="lineafinal">
-  <a href="#"><img src="../../images/flecha1.png" alt="" width="50"></a>
-</div>
-<!-- <div class="lineafinal"></div> -->
